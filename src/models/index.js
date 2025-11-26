626b26a5a7ad39c55709234a15d6caaa88a3fdec
reaja <reajamoon@pm.me>
2025-11-26 14:52:18 -0800

    oops

--- src/models/index.js
+++ src/models/index.js
@@ -5,13 +5,12 @@ dotenv.config();
 import UserModel from './User.js';
 import GuildModel from './Guild.js';
 import RecommendationModel from './Recommendation.js';
-
+import UserFicMetadataModel from './UserFicMetadata.js';
 import BirthdayMessageModel from './BirthdayMessage.js';
 import ParseQueueModel from './ParseQueue.js';
 import ParseQueueSubscriberModel from './ParseQueueSubscriber.js';
 import ConfigModel from './Config.js';
 import SeriesModel from './Series.js';
-
 import ModLockModel from './ModLock.js';
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
@@ -37,8 +36,6 @@ const BirthdayMessage = BirthdayMessageModel(sequelize);
 const ParseQueue = ParseQueueModel(sequelize);
 const ParseQueueSubscriber = ParseQueueSubscriberModel(sequelize);
 const UserFicMetadata = UserFicMetadataModel(sequelize);
-
-
 const Config = ConfigModel(sequelize);
 const Series = SeriesModel(sequelize);
 const ModLock = ModLockModel(sequelize);
@@ -50,17 +47,13 @@ ParseQueue.hasMany(ParseQueueSubscriber, { foreignKey: 'queue_id', as: 'subscrib
 ParseQueueSubscriber.belongsTo(ParseQueue, { foreignKey: 'queue_id' });
 Recommendation.belongsTo(Series, { foreignKey: 'seriesId', as: 'series' });
 Series.hasMany(Recommendation, { foreignKey: 'seriesId', as: 'works' });
-// UserFicMetadata relations
 UserFicMetadata.belongsTo(User, { foreignKey: 'userID', targetKey: 'discordId', as: 'user', constraints: false });
 // Not a true FK, but allows eager loading if needed
-
 UserFicMetadata.belongsTo(Recommendation, { foreignKey: 'ao3ID', targetKey: 'ao3ID', as: 'fic', constraints: false });
 User.hasMany(UserFicMetadata, { foreignKey: 'userID', sourceKey: 'discordId', as: 'ficMetadata' });
 Recommendation.hasMany(UserFicMetadata, { foreignKey: 'ao3ID', sourceKey: 'ao3ID', as: 'userMetadata' });
 BirthdayMessage.belongsTo(User, { foreignKey: 'userId', targetKey: 'discordId', as: 'user', constraints: false });
 User.hasMany(BirthdayMessage, { foreignKey: 'userId', sourceKey: 'discordId', as: 'birthdayMessages' });
-
-// ModLock associations
 ModLock.associate({ Recommendation, User });
 Recommendation.hasMany(ModLock, { foreignKey: 'recommendationId', as: 'modLocks' });
 
