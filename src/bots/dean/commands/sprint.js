592d1aa4906ec7244416868ea1eeab3bb6751b69
reajamoon <reajamoon@pm.me>
2025-12-04 20:05:27 -0800

    fix: nevermind it was a load bearing bracket

--- src/bots/dean/commands/sprint.js
+++ src/bots/dean/commands/sprint.js
@@ -19,7 +19,7 @@ export const data = new SlashCommandBuilder()
   .addSubcommand(sub => sub
     .setName('join')
     .setDescription('Join the active team sprint in this channel')
-    .addStringOption(opt => opt.setName('code').setDescription('Host code if multiple sprints exist'))
+    .addStringOption(opt => opt.setName('code').setDescription('Host code if multiple sprints exist')))
   .addSubcommand(sub => sub
     .setName('end')
     .setDescription('End your active sprint'))
